56d600d1cab8888176e1f0d93abf66a384a0e451
Kurtis Rainbolt-Greene <me@kurtisrainboltgreene.name>
2014-09-25 14:57:55 -0500

    Since we don't do other handlers here, fuck this

--- lib/rack/handler.rb
+++ lib/rack/handler.rb
@@ -24,37 +24,12 @@ module Rack
       raise load_error || name_error
     end
 
-    # Select first available Rack handler given an `Array` of server names.
-    # Raises `LoadError` if no handler was found.
-    #
-    #   > pick ['thin', 'webrick']
-    #   => Rack::Handler::WEBrick
-    def self.pick(server_names)
-      server_names = Array(server_names)
-      server_names.each do |server_name|
-        begin
-          return get(server_name.to_s)
-        rescue LoadError, NameError
-        end
-      end
-
-      raise LoadError, "Couldn't find handler for: #{server_names.join(', ')}."
-    end
-
     def self.default(options = {})
       # Guess.
-      if ENV.include?("PHP_FCGI_CHILDREN")
-        # We already speak FastCGI
-        options.delete :File
-        options.delete :Port
-
-        Rack::Handler::FastCGI
-      elsif ENV.include?("REQUEST_METHOD")
-        Rack::Handler::CGI
-      elsif ENV.include?("RACK_HANDLER")
-        self.get(ENV["RACK_HANDLER"])
+      if ENV.include?("RACK_HANDLER")
+        get(ENV["RACK_HANDLER"])
       else
-        pick ['thin', 'puma', 'webrick']
+        "webrick"
       end
     end
 
